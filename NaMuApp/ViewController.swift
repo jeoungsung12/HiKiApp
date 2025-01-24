ad96c839e6d6662dce262f8cab7a72b993dc950f
jeoungsung12 <jeoungsung12@naver.com>
2025-01-24 14:57:54 +0900

    Initial Commit

---
+++ NaMuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaMuApp
+//
+//  Created by 정성윤 on 1/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
